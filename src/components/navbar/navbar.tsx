c30fda2acd9b71bd6e76ded871f3f876e17fe75c
Ewe Tek Min <tekminewe@users.noreply.github.com>
2023-03-16 21:05:58 +0000

    Add container

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,7 +2,13 @@ import { forwardRef, HTMLAttributes } from 'react';
 import classNames from 'classnames';
 
 export const Navbar = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>(({ className, ...props }, ref) => {
-  return <nav {...props} ref={ref} className={classNames(className, 'flex items-center justify-between')} />;
+  return (
+    <nav
+      {...props}
+      ref={ref}
+      className={classNames(className, 'flex items-center justify-between container mx-auto')}
+    />
+  );
 });
 
 Navbar.displayName = 'Navbar';
