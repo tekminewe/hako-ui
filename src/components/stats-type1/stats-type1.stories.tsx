f98d4975c6bc0fdddf8b8e18fdb4ef0b1547166d
Ewe Tek Min <tekminewe@users.noreply.github.com>
2023-03-26 18:00:35 +0100

    Create stats

---
+++ src/components/stats-type1/stats-type1.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { StatsType1 } from './stats-type1';
+
+const meta = {
+  title: 'Components/Stats/StatsType1',
+  component: StatsType1,
+  tags: ['autodocs'],
+} satisfies Meta<typeof StatsType1>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const NoIcon: Story = {
+  args: {
+    stats: [
+      {
+        title: 'Happy Clients',
+        value: '1000+',
+      },
+      {
+        title: 'Active Projects',
+        value: '50+',
+      },
+      {
+        title: 'Year of Experiences',
+        value: '5+',
+      },
+    ],
+  },
+};
