f3f5ed79c23674579d8d5ab293d6457345fef0df
Ewe Tek Min <tekminewe@users.noreply.github.com>
2023-04-02 20:56:52 +0100

    feat: add textarea component

---
+++ packages/hako-ui/src/components/text-area/text-area.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { TextArea } from './text-area';
+
+const meta = {
+  title: 'Components/Forms/TextArea',
+  component: TextArea,
+  tags: ['autodocs'],
+} satisfies Meta<typeof TextArea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Simple: Story = {
+  args: {
+    placeholder: 'Enter your message',
+  },
+};
+
+export const WithLabel: Story = {
+  args: {
+    ...Simple.args,
+    label: 'Your message',
+  },
+};
+
+export const WithError: Story = {
+  args: {
+    ...WithLabel.args,
+    status: 'error',
+    hint: 'Your message is too long',
+  },
+};
+
+export const WithSuccess: Story = {
+  args: {
+    ...WithLabel.args,
+    status: 'success',
+    hint: 'We will get back to you soon',
+  },
+};
