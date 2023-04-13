7cad07ed280c48277312b22c2338137b722eb2f3
Ewe Tek Min <tekminewe@users.noreply.github.com>
2023-04-13 06:56:50 +0100

    style: update some styles in component

--- packages/hako-ui/src/components/hero/hero.tsx
+++ packages/hako-ui/src/components/hero/hero.tsx
@@ -2,13 +2,7 @@ import classNames from 'classnames';
 import { forwardRef, HTMLAttributes } from 'react';
 
 export const Hero = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>(({ className, ...props }, ref) => {
-  return (
-    <section
-      {...props}
-      ref={ref}
-      className={classNames(className, 'flex items-center justify-around container mx-auto h-[800px]')}
-    />
-  );
+  return <section {...props} ref={ref} className={classNames(className, 'flex container mx-auto h-[800px]')} />;
 });
 
 Hero.displayName = 'Hero';
