f8d069327aaa5f4cf0d4db509e78b5312f2b291c
Ewe Tek Min <tekminewe@users.noreply.github.com>
2023-04-23 07:14:52 +0100

    feat: add missing props in NavbarType1

--- packages/hako-ui/src/components/navbar-type1/navbar-type1.tsx
+++ packages/hako-ui/src/components/navbar-type1/navbar-type1.tsx
@@ -2,9 +2,8 @@ import { forwardRef, useState } from 'react';
 import { TfiClose } from 'react-icons/tfi';
 import { Navbar } from '../navbar/navbar';
 import { NavbarBrand } from '../navbar/navbar-brand';
-import { NavbarLinks } from '../navbar/navbar-links';
+import { NavbarLinks, NavbarLinksProps } from '../navbar/navbar-links';
 import { NavbarToggle } from '../navbar/navbar-toggle';
-import classNames from 'classnames';
 import { Drawer } from '../drawer';
 
 export interface NavbarType1Props {
@@ -31,6 +30,21 @@ export interface NavbarType1Props {
    * @required
    */
   links: string[];
+
+  /**
+   * The callback function to be called when a link is clicked
+   * @type NavbarLinksProps['onLinkClick']
+   * @example () => console.log("Link clicked")
+   */
+  onLinkClick?: NavbarLinksProps['onLinkClick'];
+
+  /**
+   * The index of the selected link
+   * @default undefined
+   * @type number
+   * @example 0
+   */
+  selectedIndex?: number;
 }
 
 export const NavbarType1 = forwardRef<HTMLElement, NavbarType1Props>((props, ref) => {
@@ -48,7 +62,12 @@ export const NavbarType1 = forwardRef<HTMLElement, NavbarType1Props>((props, ref
     <>
       <Navbar ref={ref} className="justify-between">
         <NavbarBrand logo={props.logo} title={props.title} />
-        <NavbarLinks links={props.links} className="hidden lg:flex" />
+        <NavbarLinks
+          links={props.links}
+          className="hidden lg:flex"
+          onLinkClick={props.onLinkClick}
+          selectedIndex={props.selectedIndex}
+        />
         <NavbarToggle containerClassName="lg:hidden" onClick={handleToggle} />
       </Navbar>
       <Drawer anchor="right" className="lg:hidden p-4" open={show} onClose={handleClose}>
