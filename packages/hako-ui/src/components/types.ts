2609833282e1374cceaa396199796c92fdbe1c23
Ewe Tek Min <tekminewe@users.noreply.github.com>
2023-06-02 08:15:45 +0100

    feat: add row

--- packages/hako-ui/src/components/types.ts
+++ packages/hako-ui/src/components/types.ts
@@ -10,3 +10,5 @@ export interface PluginOptions {
 }
 
 export type PluginCreator = (options?: PluginOptions) => typeof plugin;
+
+export type LayoutColumn = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
---
+++ packages/hako-ui/src/utils/size.ts
@@ -0,0 +1,173 @@
+export const xsLayoutColumnToClassName = (size: number) => {
+  switch (size) {
+    case 1:
+      return 'w-1/12';
+    case 2:
+      return 'w-1/6';
+    case 3:
+      return 'w-1/4';
+    case 4:
+      return 'w-1/3';
+    case 5:
+      return 'w-5/12';
+    case 6:
+      return 'w-1/2';
+    case 7:
+      return 'w-7/12';
+    case 8:
+      return 'w-2/3';
+    case 9:
+      return 'w-3/4';
+    case 10:
+      return 'w-5/6';
+    case 11:
+      return 'w-11/12';
+    default:
+      return 'w-full';
+  }
+};
+
+export const smLayoutColumnToClassName = (size: number) => {
+  switch (size) {
+    case 1:
+      return 'sm:w-1/12';
+    case 2:
+      return 'sm:w-1/6';
+    case 3:
+      return 'sm:w-1/4';
+    case 4:
+      return 'sm:w-1/3';
+    case 5:
+      return 'sm:w-5/12';
+    case 6:
+      return 'sm:w-1/2';
+    case 7:
+      return 'sm:w-7/12';
+    case 8:
+      return 'sm:w-2/3';
+    case 9:
+      return 'sm:w-3/4';
+    case 10:
+      return 'sm:w-5/6';
+    case 11:
+      return 'sm:w-11/12';
+    default:
+      return 'sm:w-full';
+  }
+};
+
+export const mdLayoutColumnToClassName = (size: number) => {
+  switch (size) {
+    case 1:
+      return 'md:w-1/12';
+    case 2:
+      return 'md:w-1/6';
+    case 3:
+      return 'md:w-1/4';
+    case 4:
+      return 'md:w-1/3';
+    case 5:
+      return 'md:w-5/12';
+    case 6:
+      return 'md:w-1/2';
+    case 7:
+      return 'md:w-7/12';
+    case 8:
+      return 'md:w-2/3';
+    case 9:
+      return 'md:w-3/4';
+    case 10:
+      return 'md:w-5/6';
+    case 11:
+      return 'md:w-11/12';
+    default:
+      return 'md:w-full';
+  }
+};
+
+export const lgLayoutColumnToClassName = (size: number) => {
+  switch (size) {
+    case 1:
+      return 'lg:w-1/12';
+    case 2:
+      return 'lg:w-1/6';
+    case 3:
+      return 'lg:w-1/4';
+    case 4:
+      return 'lg:w-1/3';
+    case 5:
+      return 'lg:w-5/12';
+    case 6:
+      return 'lg:w-1/2';
+    case 7:
+      return 'lg:w-7/12';
+    case 8:
+      return 'lg:w-2/3';
+    case 9:
+      return 'lg:w-3/4';
+    case 10:
+      return 'lg:w-5/6';
+    case 11:
+      return 'lg:w-11/12';
+    default:
+      return 'lg:w-full';
+  }
+};
+
+export const xlLayoutColumnToClassName = (size: number) => {
+  switch (size) {
+    case 1:
+      return 'xl:w-1/12';
+    case 2:
+      return 'xl:w-1/6';
+    case 3:
+      return 'xl:w-1/4';
+    case 4:
+      return 'xl:w-1/3';
+    case 5:
+      return 'xl:w-5/12';
+    case 6:
+      return 'xl:w-1/2';
+    case 7:
+      return 'xl:w-7/12';
+    case 8:
+      return 'xl:w-2/3';
+    case 9:
+      return 'xl:w-3/4';
+    case 10:
+      return 'xl:w-5/6';
+    case 11:
+      return 'xl:w-11/12';
+    default:
+      return 'xl:w-full';
+  }
+};
+
+export const xxlLayoutColumnToClassName = (size: number) => {
+  switch (size) {
+    case 1:
+      return '2xl:w-1/12';
+    case 2:
+      return '2xl:w-1/6';
+    case 3:
+      return '2xl:w-1/4';
+    case 4:
+      return '2xl:w-1/3';
+    case 5:
+      return '2xl:w-5/12';
+    case 6:
+      return '2xl:w-1/2';
+    case 7:
+      return '2xl:w-7/12';
+    case 8:
+      return '2xl:w-2/3';
+    case 9:
+      return '2xl:w-3/4';
+    case 10:
+      return '2xl:w-5/6';
+    case 11:
+      return '2xl:w-11/12';
+    default:
+      return '2xl:w-full';
+  }
+};
